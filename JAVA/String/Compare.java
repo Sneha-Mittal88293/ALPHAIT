d254a7bf080a2e1dca703d05ece78eb1c09beb2a
sneha <snehamittal88293@gmail.com>
2023-03-02 21:47:17 +0530

    compare string

---
+++ JAVA/String/Compare.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Compare{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = new String("abcd");
+        String str2 = "abcd";
+        System.out.print(str.compareTo(str2));
+        System.out.print(str.equals(str2));
+
+
+        // if(str == str2){
+        //     System.out.print("equal");
+        // }
+        // else{
+        //      System.out.print(" not equal");
+        // }
+    }
+}
