3f5d88e91120c1c9b3d73fb1a4854da28149f98f
sneha <snehamittal88293@gmail.com>
2023-03-22 16:31:35 +0530

    left and right shift

---
+++ JAVA/Bit Manipulation/BinaryLeftShift.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class BinaryLeftShift{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Left shift " + (5<<2));
+         System.out.println("Right shift " + (5>>2));
+    }
+}
