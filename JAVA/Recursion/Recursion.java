40765dec2803fb6d798226f9d1e3a1edf3c40cd5
sneha <snehamittal88293@gmail.com>
2023-04-04 01:07:46 +0530

    print increasing or decreasing order

--- JAVA/Recursion/Recursion.java
+++ JAVA/Recursion/Recursion.java
@@ -1,21 +1,32 @@
 import java.util.*;
 
 class Recursion{
-    public static void func(int n){
+    public static void decfunc(int n){
         if(n==0){
-             System.out.print(n);
+             System.out.println(n);
              return;
         }
 
             System.out.print(n+" ");
-            func(n-1);
-            }
+            decfunc(n-1);
+    }
+    public static void incfunc(int n){
+        if(n == 0){
+            System.out.print(n + " ");
+            return;
+        }
+        incfunc(n-1);
+        System.out.print(n+" ");
+
+    }
     
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
          int n = sc.nextInt();
          
-         func(n);
+         decfunc(n);
+         incfunc(n);
+
     }
 }
 
