0904d258c48a17389c6d1498cb2aef69c21d739c
sneha <snehamittal88293@gmail.com>
2023-04-17 17:20:12 +0530

    power function

--- JAVA/Recursion/Power.java
+++ JAVA/Recursion/Power.java
@@ -5,9 +5,21 @@ class Power{
         }
         return x * pow(x ,n-1);
     }
+
+    public static int pow2(int x , int n){
+        if( n == 0){
+        return 1;
+         }
+        if( n  % 2 == 0){
+            return pow(x , (n/2)) * pow(x , (n/2));
+        }
+      
+      return x * pow(x , (n/2)) * pow(x , (n/2));
+    }
     public static void main(String args[]){
         int x = 2;
-        int n = 10;
-        System.out.print(pow(x ,n));
+        int n = 9;
+        System.out.println(pow(x ,n));
+        System.out.print(pow2(x,n));
     }
 }
