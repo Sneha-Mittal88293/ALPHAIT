de12d4796678896c8d591f20cfa8e28bfe65f866
sneha <snehamittal88293@gmail.com>
2023-05-03 20:18:47 +0530

    quickSort

---
+++ JAVA/Divide Conquer/QuickSort.java
@@ -0,0 +1,47 @@
+class QuickSort{
+    public static void printArr(int arr[]){
+        for(int i=0 ; i<arr.length ; i++){
+           System.out.print(arr[i] + " ");
+        }   
+    }
+    public static void quickSort(int arr[] , int si , int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int pIdx = partition(arr , si , ei);
+        quickSort(arr , si , pIdx-1);  //left
+        quickSort(arr , pIdx+1 , ei);   //right
+
+
+    }
+    public static int partition(int arr[] , int si , int ei){
+        int pivot = arr[ei];
+        int i = si-1;  //to make place for element smaller than pivot
+        
+
+        for(int j =si ; j <ei ; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        //for pivot
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;  //here i the index of pivot
+
+
+    }
+     public static void main(String args[]){
+        int arr[] = {1,4,2,3,6,1,7,3,9,5,7};
+        quickSort(arr, 0 , arr.length-1);
+        printArr(arr);
+
+     }
+}
